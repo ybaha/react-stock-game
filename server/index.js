dd9824dc54f48c01041cc63fec6e0ef93b8c152b
ybaha <ybahaerarslan@gmail.com>
2020-10-14 11:21:37 +0300

    fixed cors policy

--- server/index.js
+++ server/index.js
@@ -5,71 +5,108 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 const path = require('path')
 const { json } = require('express')
-const infoJs = require('./info')
+const info = require('./info.json')
+const cors = require('cors')
+
 
 let PORT = process.env.PORT || 5000;
 
-const API_KEY = 'HGJWFG4N8AQ66ICD';
+const API_KEY = 'HGJWFG4N8AQ66ICD'
+const API_KEY2 = 'VKJ2JYJCM1XKCA1G'
 
 // let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`
 
 // const first = require(__dirname + '/stocks/bir.js')
 
+app.use(express.static("stocks"))
+app.use(cors())
 
 
-// for (let i = 0; i < slug.length; i++) {
-//   app.get(`/${slug[i]}`, (req, res, next) => {
-//     console.log("selam buradasin =>" + slug[i])
-//     res.json(require(__dirname + `/stocks/${slug[i]}.json`))
-//     fs.writeFileSync(path.resolve(__dirname + '/stocks/iki.json'), JSON.stringify(student))
+const setRoutes = () => {
+  let slug = JSON.parse(fs.readFileSync('./info.json', 'utf8'))
+  console.log(slug);
+  for (let i = 0; i < slug.length; i++) {
+    app.get(`/${slug[i]}`, (req, res, next) => {
+      console.log("selam buradasin =>" + slug[i])
+      res.json(require(__dirname + `/stocks/${slug[i]}.json`))
+      // fs.writeFileSync(path.resolve(__dirname + '/stocks/iki.json'), JSON.stringify(student))
 
-//   })
-// }
+    })
+  }
+}
+
+setRoutes()
+
+
+
 
 
 const fetchStockData = (slug) => {
   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${slug}&outputsize=compact&apikey=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
-      console.table(data)
-      fs.writeFileSync(path.resolve(__dirname + `/stocks/${slug}.json`), JSON.stringify(data))
+      if("merchant_id" in data){
+
+      }
+      else{
+        fs.writeFileSync(path.resolve(__dirname + `/stocks/${slug}.json`), JSON.stringify(data))
+      }
     })
     .catch(err => console.log(err))
 }
 
 
+const refreshStockData = () => {
+  let stocks = JSON.parse(fs.readFileSync('./info.json', 'utf8'))
+  stocks.forEach( (stock, index) => {
+    setTimeout( () => {
+      console.log(`${stock} updated`)
+      fetchStockData(stock)
+    }, 14000 * index)
+  })
+
+}
+
+refreshStockData()
+
+
+
+
+
 const getAllStockNames = (folder) => {
   let stcks = []
   fs.readdirSync(folder).forEach(file => {
-      let fileName
-      fileName = file.slice(0, -5)
-      stcks.push(fileName)
-      console.log(fileName)
-    
-    })
+    let fileName
+    fileName = file.slice(0, -5)
+    stcks.push(fileName)
+    //console.log(fileName)
 
-    fs.writeFileSync(path.resolve(__dirname + '/info.js'), JSON.stringify(stcks))
+  })
+
+  fs.writeFileSync(path.resolve(__dirname + '/info.js'), JSON.stringify(stcks))
 
   return stcks
 }
 
+//getAllStockNames('./stocks')
 
-let direc = "./stocks"
-let allStocks = []
+// let direc = "./stocks"
+// let allStocks = []
 
-allStocks = getAllStockNames(direc, allStocks)
-
-
-
+// allStocks = getAllStockNames(direc, allStocks)
 
 
 
 let appendArray = ["NKE", "KO", "ADBE", "MCD", "PEP"]
 const appendNewStocks = (array) => {
   for (let i = 0; i < array.length; i++) {
-    fs.appendFile(`stocks/${array[i]}.json`, json , (err) => {
+
+    fs.appendFile(`stocks/${array[i]}.json`, json, (err) => {
       if (err) throw err
-      console.log('The "data to append" was appended to file!')
+      else{
+        console.log('The "data to append" was appended to file!')
+
+      }
     })
   }
 }
@@ -78,10 +115,6 @@ const appendNewStocks = (array) => {
 
 
 
-
-
-
-app.use(express.static("stocks"))
 
 
 app.get("/", (req, res, next) => {
