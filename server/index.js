81843d9c58fd6ff7798268fa75cf2e755058eee8
ybaha <ybahaerarslan@gmail.com>
2020-10-15 20:16:11 +0300

    fixed server functions

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ let http = require('http').createServer(app)
 const fetch = require('node-fetch')
 const fs = require('fs')
 const path = require('path')
-const { json } = require('express')
-const info = require('./info.json')
 const cors = require('cors')
 
 
@@ -14,16 +12,13 @@ let PORT = process.env.PORT || 5000;
 const API_KEY = 'HGJWFG4N8AQ66ICD'
 const API_KEY2 = 'VKJ2JYJCM1XKCA1G'
 
-// let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`
-
-// const first = require(__dirname + '/stocks/bir.js')
 
 app.use(express.static("stocks"))
 app.use(cors())
 
 
 const setRoutes = () => {
-  let slug = JSON.parse(fs.readFileSync('./info.json', 'utf8'))
+  let slug = JSON.parse(fs.readFileSync('./info/info.json', 'utf8'))
   console.log(slug);
   for (let i = 0; i < slug.length; i++) {
     app.get(`/${slug[i]}`, (req, res, next) => {
@@ -35,9 +30,6 @@ const setRoutes = () => {
   }
 }
 
-setRoutes()
-
-
 
 
 
@@ -45,10 +37,10 @@ const fetchStockData = (slug) => {
   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${slug}&outputsize=compact&apikey=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
-      if("merchant_id" in data){
+      if ("merchant_id" in data) {
 
       }
-      else{
+      else {
         fs.writeFileSync(path.resolve(__dirname + `/stocks/${slug}.json`), JSON.stringify(data))
       }
     })
@@ -57,9 +49,9 @@ const fetchStockData = (slug) => {
 
 
 const refreshStockData = () => {
-  let stocks = JSON.parse(fs.readFileSync('./info.json', 'utf8'))
-  stocks.forEach( (stock, index) => {
-    setTimeout( () => {
+  let stocks = JSON.parse(fs.readFileSync('./info/info.json', 'utf8'))
+  stocks.forEach((stock, index) => {
+    setTimeout(() => {
       console.log(`${stock} updated`)
       fetchStockData(stock)
     }, 14000 * index)
@@ -67,61 +59,61 @@ const refreshStockData = () => {
 
 }
 
-refreshStockData()
 
 
 
-
-
-const getAllStockNames = (folder) => {
-  let stcks = []
+const getAllStockNames = (folder, stcks) => {
   fs.readdirSync(folder).forEach(file => {
     let fileName
     fileName = file.slice(0, -5)
     stcks.push(fileName)
     //console.log(fileName)
-
   })
 
-  fs.writeFileSync(path.resolve(__dirname + '/info.js'), JSON.stringify(stcks))
-
-  return stcks
+  fs.writeFileSync(path.resolve(__dirname + '/info/info.json'), JSON.stringify(stcks))
 }
 
-//getAllStockNames('./stocks')
 
-// let direc = "./stocks"
 // let allStocks = []
 
 // allStocks = getAllStockNames(direc, allStocks)
 
 
 
-let appendArray = ["NKE", "KO", "ADBE", "MCD", "PEP"]
+
 const appendNewStocks = (array) => {
-  for (let i = 0; i < array.length; i++) {
 
-    fs.appendFile(`stocks/${array[i]}.json`, json, (err) => {
-      if (err) throw err
-      else{
-        console.log('The "data to append" was appended to file!')
+  let currentStocks = []
+  getAllStockNames("./stocks", currentStocks)
 
-      }
-    })
-  }
+  array.forEach((e) => {
+    if (!currentStocks.includes(e)) {
+      fs.appendFile(`stocks/${e}.json`, "", (err) => {
+        if (err) console.log(err)
+        else {
+          console.log(`${e} appended to stocks`)
+        }
+      })
+    }
+  })
+
 }
 
-//appendNewStocks(appendArray)
+let appendArray = ["KO", "AMZN", "BABA", "FORD"]
 
+appendNewStocks(appendArray) 
 
+setRoutes() 
+
+refreshStockData() 
 
 
 
 app.get("/", (req, res, next) => {
-  res.json(getAllStockNames('stocks'));
-})
+    res.json(getAllStockNames('stocks'));
+  })
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on: ${PORT}`);
-})
+    console.log(`Server running on: ${PORT}`);
+  })
