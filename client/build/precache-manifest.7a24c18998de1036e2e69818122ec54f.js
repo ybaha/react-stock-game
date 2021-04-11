f474b857883c174b1b5242d46ca990925ab65293
ybaha <58085088+ybaha@users.noreply.github.com>
2021-04-12 00:03:48 +0300

    build

--- client/build/precache-manifest.7a24c18998de1036e2e69818122ec54f.js
+++ client/build/precache-manifest.7a24c18998de1036e2e69818122ec54f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "211a41db7d49fa351e2c823982538b72",
+    "revision": "7303dd2c91c5436b9b0e86d2f57e08d5",
     "url": "/react-stock-game/index.html"
   },
   {
-    "revision": "1fc26c68fa99dcd7fd77",
+    "revision": "84e8a7357a4e2cd1e5e8",
     "url": "/react-stock-game/static/css/main.962e70b8.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-stock-game/static/js/2.6b82a255.chunk.js"
   },
   {
-    "revision": "1fc26c68fa99dcd7fd77",
-    "url": "/react-stock-game/static/js/main.d1841701.chunk.js"
+    "revision": "84e8a7357a4e2cd1e5e8",
+    "url": "/react-stock-game/static/js/main.c218b7c6.chunk.js"
   },
   {
     "revision": "44f929a61c96cace1fa8",
