47659fd5f678956d080f9683ec3c12ef3c485c23
ybaha <58085088+ybaha@users.noreply.github.com>
2021-04-11 23:49:06 +0300

    added client prod build

---
+++ client/build/precache-manifest.08b5bb56c3be55573a04be5d4bf3ae45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211a41db7d49fa351e2c823982538b72",
+    "url": "/react-stock-game/index.html"
+  },
+  {
+    "revision": "1fc26c68fa99dcd7fd77",
+    "url": "/react-stock-game/static/css/main.962e70b8.chunk.css"
+  },
+  {
+    "revision": "1d1251305aa3f5b4eb86",
+    "url": "/react-stock-game/static/js/2.6b82a255.chunk.js"
+  },
+  {
+    "revision": "1fc26c68fa99dcd7fd77",
+    "url": "/react-stock-game/static/js/main.d1841701.chunk.js"
+  },
+  {
+    "revision": "44f929a61c96cace1fa8",
+    "url": "/react-stock-game/static/js/runtime~main.d6898772.js"
+  }
+]);
